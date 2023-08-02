ba49be950f5fd7a1eacb88a74989f1abe179f265
unknown <wayneklyde@gmail.com>
2023-08-02 18:47:11 +0300

    Adding views with key word arguments

--- polls/views.py
+++ polls/views.py
@@ -4,5 +4,27 @@ from django.http import HttpResponse
 # Create your views here.
 
 # First view
+
 def index(request):
+    '''
+    Index class and page displays the latest few questions
+    '''
     return HttpResponse("First view")
+
+def detail(request, question_id):
+    '''
+    Detail class and page displays the question text witout results but with a form to vote
+    '''
+    return HttpResponse("This is question %s. " % question_id)
+
+def results(request, question_id):
+    '''
+    Results class or page displays results of a particular question
+    '''
+    return HttpResponse("You are looking at results of question %s. " % question_id)
+
+def vote(request, question_id):
+    '''
+    The vote action classs or page handles voting for a particular choice in a particular question
+    '''
+    return HttpResponse("You are voting on question %s. " % question_id)
